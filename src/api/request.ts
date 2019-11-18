575bc1a779bfc8fd1c5350e0ede8b0222afd87b7
Wildlifes <2632003786@qq.com>
2019-11-18 21:07:20 +0800

    :sparkles: update mv player

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { message } from "antd";
-import store from "@/store";
+// import { message } from "antd";
+// import store from "@/store";
 import { getToken } from "@/utils/auth";
 
-const $message = message;
+// const $message = message;
 
 const customHeaderToken = "token"; //自定义同步token名称
 
@@ -19,12 +19,12 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
       config.headers[customHeaderToken] = getToken();
-    }
+    // }
     return config;
   },
   error => {
@@ -51,16 +51,6 @@ service.interceptors.response.use(
 
     // if the custom code is not 200, it is judged as an error.
     if (res.code !== 200) {
-      // $message({
-      //   message: res.message || 'Error',
-      //   type: 'error',
-      //   duration: 5 * 1000
-      // })
-
-      // // 500: Illegal token
-      // store.dispatch('user/resetToken').then(() => {
-      //   location.reload()
-      // })
       return Promise.reject(new Error(res.message || "Error"));
     } else {
       return res;
@@ -70,20 +60,20 @@ service.interceptors.response.use(
     const errData = error.response ? error.response.data : {};
 
     // 500: Illegal token
-    if (errData.message && errData.message.startsWith("700,")) {
-      $message({
-        message: errData.message || "",
-        type: "error",
-        duration: 5 * 1000
-      });
-      setTimeout(() => {
-        store.dispatch("user/resetToken").then(() => {
-          setTimeout(() => {
-            location.reload();
-          }, 1000);
-        });
-      }, 1000);
-    }
+    // if (errData.message && errData.message.startsWith("700,")) {
+    //   $message({
+    //     message: errData.message || "",
+    //     type: "error",
+    //     duration: 5 * 1000
+    //   });
+    //   setTimeout(() => {
+    //     store.dispatch("user/resetToken").then(() => {
+    //       setTimeout(() => {
+    //         location.reload();
+    //       }, 1000);
+    //     });
+    //   }, 1000);
+    // }
 
     return Promise.reject(errData);
   }
